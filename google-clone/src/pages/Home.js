be50084a0f60296d71c43436cd657e70330ec87f
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-10-05 01:12:31 +0500

    .

---
+++ google-clone/src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='home'>
+
+      <div className='home_header'>
+
+      </div>
+
+      <div className='home_body'>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Home
