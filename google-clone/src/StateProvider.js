ea08fa3ac643756026f57d0212df6721418a525c
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-10-06 01:12:51 +0500

    data layer updated

--- google-clone/src/StateProvider.js
+++ google-clone/src/StateProvider.js
@@ -1,12 +1,13 @@
 import React, { createContext, useContext, useReducer } from 'react'
+
 export const StateContext = createContext();
 
-export const StateProvider = ( {reducer, initialState, children} ) => {
+export const StateProvider = ( {reducer, initialState, children} ) => (
 
     <StateContext.Provider value={useReducer(reducer, initialState)}>
         {children}
     </StateContext.Provider>
-}
+)
 
 //Hook that allows us to pull info from data layer
 export const useStateValue = () => useContext(StateContext);
